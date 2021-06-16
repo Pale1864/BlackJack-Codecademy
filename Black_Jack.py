b958059a3c1db9ec546cb4a31bc3996e18acea65
Pale <brodstar_3000@hotmail.com>
2021-06-16 17:23:21 +1000

    Game Intro written

---
+++ Black_Jack.py
@@ -0,0 +1,9 @@
+play = input("Would you like to play a game of Black Jack? (y/n)")
+if play == "y":
+    print("Welcome to Pale's Black Jack table! Let's get started.")
+elif play == "n":
+    print("You have chosen not to play, goodbye")
+    pass
+else:
+    print("You have entered an incorrect value")
+    pass
